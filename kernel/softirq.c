4b96033d84687a4b28eb5de0a37eaa742d64350d
Andi Kleen <ak@linux.intel.com>
2012-09-16 12:16:48 -0700

    sched, txn: Disable transactions for the scheduler
    Context switch does various operations that have a tendency to
    abort (MSR writes, XSAVE etc.) Disable transactions for the scheduler
    locks for now when running in the context switch.
    
    wake_up() and load balancing etc. can be still elided though.
    
    This is also slightly safer for real time applications.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- kernel/softirq.c
+++ kernel/softirq.c
@@ -319,6 +319,8 @@ void irq_enter(void)
 	}
 
 	__irq_enter();
+	/* For now */
+	disable_txn();	
 }
 
 static inline void invoke_softirq(void)
@@ -361,6 +363,7 @@ void irq_exit(void)
 
 	tick_irq_exit();
 	rcu_irq_exit();
+	reenable_txn();
 }
 
 /*
