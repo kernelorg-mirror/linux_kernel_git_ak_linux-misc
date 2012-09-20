611e86174944a331845d460049553c624fbd0d13
Andi Kleen <ak@linux.intel.com>
2012-09-19 17:24:11 -0700

    perf, tools: Add support for record transaction flags
    Add the glue in the user tools to record transaction flags with
    --transaction (-T was already taken) and dump them.
    
    Followon patches will use them.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- tools/perf/util/evsel.c
+++ tools/perf/util/evsel.c
@@ -420,6 +420,9 @@ void perf_evsel__config(struct perf_evsel *evsel, struct perf_record_opts *opts,
 	if (opts->sample_weight)
 		attr->sample_type	|= PERF_SAMPLE_WEIGHT;
 
+	if (opts->sample_transaction)
+		attr->sample_type	|= PERF_SAMPLE_TRANSACTION;
+
 	if (opts->call_graph)
 		attr->sample_type	|= PERF_SAMPLE_CALLCHAIN;
 
@@ -873,6 +876,12 @@ int perf_evsel__parse_sample(struct perf_evsel *evsel, union perf_event *event,
 	data->weight = 0;
 	if (type & PERF_SAMPLE_WEIGHT) {
 		data->weight = *array;
+		array++;
+	}
+
+	data->transaction = 0;
+	if (type & PERF_SAMPLE_TRANSACTION) {
+		data->transaction = *array;
 		array++;
 	}
 
