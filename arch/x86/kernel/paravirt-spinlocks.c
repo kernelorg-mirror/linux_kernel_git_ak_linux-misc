1fc4f048eb0e19f6ca5cc7861bd687db63a8d57f
Andi Kleen <ak@linux.intel.com>
2012-10-10 15:07:59 -0700

    x86, paravirt: Add support for arch_spin_unlock_flags/irq
    Add support for arch_spin_unlock_flags/irq
    
    Only done for the paravirt case, non paravirt just uses the wrappers
    in the upper level code.
    
    Cc: jeremy@goop.org
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- arch/x86/kernel/paravirt-spinlocks.c
+++ arch/x86/kernel/paravirt-spinlocks.c
@@ -13,6 +13,20 @@ default_spin_lock_flags(arch_spinlock_t *lock, unsigned long flags)
 	arch_spin_lock(lock);
 }
 
+static void
+default_spin_unlock_flags(arch_spinlock_t *lock, unsigned long flags)
+{
+	arch_spin_unlock(lock);
+	local_irq_restore(flags);
+}
+
+static inline void
+default_spin_unlock_irq(arch_spinlock_t *lock)
+{
+	arch_spin_unlock(lock);
+	local_irq_enable();
+}
+
 struct pv_lock_ops pv_lock_ops = {
 #ifdef CONFIG_SMP
 	.spin_is_locked = __ticket_spin_is_locked,
@@ -22,6 +36,8 @@ struct pv_lock_ops pv_lock_ops = {
 	.spin_lock_flags = default_spin_lock_flags,
 	.spin_trylock = __ticket_spin_trylock,
 	.spin_unlock = __ticket_spin_unlock,
+	.spin_unlock_irq = default_spin_unlock_flags,
+	.spin_unlock_flags = default_spin_unlock_irq,
 #endif
 };
 EXPORT_SYMBOL(pv_lock_ops);
