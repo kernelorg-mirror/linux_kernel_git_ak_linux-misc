def9f4a63e4583a6887ab66d623cb7155102e02a
Andi Kleen <ak@linux.intel.com>
2013-05-29 08:30:44 -0700

    mm: Disable lock elision for zap pte range
    zap pte range tends to exceed the transaction capacity, while
    walking the page tables under lock.
    
    Since the lock is all shared with other VM paths, this is a tough
    situation to handle for the automatic adaptation, while relies
    on per lock state.
    
    So disable elision for it explicitly.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- mm/memory.c
+++ mm/memory.c
@@ -1078,6 +1078,8 @@ static unsigned long zap_pte_range(struct mmu_gather *tlb,
 
 again:
 	init_rss_vec(rss);
+
+	disable_txn();	/* Likely to exceed capacity */
 	start_pte = pte_offset_map_lock(mm, pmd, addr, &ptl);
 	pte = start_pte;
 	arch_enter_lazy_mmu_mode();
@@ -1173,6 +1175,7 @@ again:
 	add_mm_rss_vec(mm, rss);
 	arch_leave_lazy_mmu_mode();
 	pte_unmap_unlock(start_pte, ptl);
+	reenable_txn();
 
 	/*
 	 * mmu_gather ran out of room to batch pages, we break out of
