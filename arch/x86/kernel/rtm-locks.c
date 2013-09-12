14e9e121ec4c4badd9931139aecbab05baf7f186
Andi Kleen <ak@linux.intel.com>
2013-09-12 14:37:19 -0700

    locking, tsx: Add adaptive elision to mutexes
    Add adaptive elision to the mutex code through the standard
    elision macros. mutex_is_locked() has to abort, as elision
    cannot answer that question. We add a short to struct mutex
    to store the adaptation state.
    
    I left the wound-wind mutexes alone for now, not sure
    they are that common.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- arch/x86/kernel/rtm-locks.c
+++ arch/x86/kernel/rtm-locks.c
@@ -433,6 +433,10 @@ module_param(rwsem_elision, static_key, 0644);
 DEFINE_ELISION_CONFIG(, readsem,  readsem_elision_config);
 DEFINE_ELISION_CONFIG(, writesem, writesem_elision_config);
 
+struct static_key mutex_elision = STATIC_KEY_INIT_FALSE;
+module_param(mutex_elision, static_key, 0644);
+DEFINE_ELISION_CONFIG(, mutex, mutex_elision_config);
+
 void __init init_rtm_spinlocks(void)
 {
 	if (!boot_cpu_has(X86_FEATURE_RTM))
@@ -462,8 +466,10 @@ void __init init_rtm_spinlocks(void)
 	static_key_slow_inc(&rwlock_elision);
 	static_key_slow_inc(&bitlock_elision);
 	static_key_slow_inc(&rwsem_elision);
+	static_key_slow_inc(&mutex_elision);
 }
 
 struct static_key bitlock_elision = STATIC_KEY_INIT_FALSE;
 EXPORT_SYMBOL(bitlock_elision);
 module_param(bitlock_elision, static_key, 0644);
+
