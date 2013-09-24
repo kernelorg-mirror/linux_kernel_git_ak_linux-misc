2d8aaaa60ea9caf6cffe028e52c4ef9e20d11d9b
Andi Kleen <ak@linux.intel.com>
2013-09-24 13:00:01 -0700

    locking, tsx: Add generic support for stateful rwsem
    In principle lock elision allows readers to speculate independently
    (that is not check if any other reader took the lock).
    It only needs to check for locked writers.
    
    However the usual trick of checking the lock free to decide
    whether the lock is eliding or not doesn't work
    
    We can handle this by passing an explicit elision state around.
    The state has be passed from lock to unlock. Any stateful lock
    has to be matched with a stateful unlock, but other users
    of the same lock can use non stateful accesses.
    
    We still have to read the r/w count to check for writers,
    so parallel lockers or unlockers may still abort, if their
    time window overlaps.
    
    This patch adds the generic interface to do this for reader sems.
    The actual code is enabled in the architecture.
    
    The primary target is mmap_sem.
    
    Without lock elision this is a nop.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- include/linux/lockdep.h
+++ include/linux/lockdep.h
@@ -431,6 +431,16 @@ do {								\
 	lock_acquired(&(_lock)->dep_map, _RET_IP_);			\
 } while (0)
 
+#define LOCK_CONTENDED_STATE(_lock, try, lock, state)		\
+do {								\
+	if (!try(_lock, state)) {				\
+		lock_contended(&(_lock)->dep_map, _RET_IP_);	\
+		lock(_lock, state);				\
+	}							\
+	lock_acquired(&(_lock)->dep_map, _RET_IP_);		\
+} while (0)
+
+
 #else /* CONFIG_LOCK_STAT */
 
 #define lock_contended(lockdep_map, ip) do {} while (0)
@@ -438,6 +448,9 @@ do {								\
 
 #define LOCK_CONTENDED(_lock, try, lock) \
 	lock(_lock)
+
+#define LOCK_CONTENDED_STATE(_lock, try, lock, state) \
+	lock(_lock, state)
 
 #endif /* CONFIG_LOCK_STAT */
 
