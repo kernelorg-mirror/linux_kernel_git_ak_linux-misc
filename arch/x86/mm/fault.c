4b4a52ec617b4724619ab772542d115f63011682
Andi Kleen <ak@linux.intel.com>
2013-09-24 14:34:59 -0700

    locking, tsx, mm: Implement stateful elision for mmap_sem readers
    Support mmap_sem readers speculating (mostly) independently. Even
    when a reader aborts other CPUs can still (mostly) speculate.
    
    This requires using down/up_read_state and friends in the x86
    fault handler.  This is fairly straight forward for the common
    case
    
    I cheated a bit on errors and simply abort in this case.
    
    Another special case is the unlock and retry case hidden deep in
    filemap.c. The up_read there has to know the correct state.
    To handle this we pass the elision state as a new flags bit.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- arch/x86/mm/fault.c
+++ arch/x86/mm/fault.c
@@ -785,6 +785,9 @@ __bad_area(struct pt_regs *regs, unsigned long error_code,
 {
 	struct mm_struct *mm = current->mm;
 
+	/* Forget about elision state on error. */
+	elide_abort();
+
 	/*
 	 * Something tried to access memory that isn't in our memory map..
 	 * Fix it, but check if it's kernel or user first..
@@ -846,6 +849,8 @@ static noinline void
 mm_fault_error(struct pt_regs *regs, unsigned long error_code,
 	       unsigned long address, unsigned int fault)
 {
+	elide_abort(); /* Forget about elision state for errors */
+
 	if (fatal_signal_pending(current) && !(error_code & PF_USER)) {
 		up_read(&current->mm->mmap_sem);
 		no_context(regs, error_code, address, 0, 0);
@@ -1003,6 +1008,7 @@ __do_page_fault(struct pt_regs *regs, unsigned long error_code)
 	struct mm_struct *mm;
 	int fault;
 	unsigned int flags = FAULT_FLAG_ALLOW_RETRY | FAULT_FLAG_KILLABLE;
+	int eliding;
 
 	tsk = current;
 	mm = tsk->mm;
@@ -1120,14 +1126,14 @@ __do_page_fault(struct pt_regs *regs, unsigned long error_code)
 	 * validate the source. If this is invalid we can skip the address
 	 * space check, thus avoiding the deadlock:
 	 */
-	if (unlikely(!down_read_trylock(&mm->mmap_sem))) {
+	if (unlikely(!down_read_trylock_state(&mm->mmap_sem, &eliding))) {
 		if ((error_code & PF_USER) == 0 &&
 		    !search_exception_tables(regs->ip)) {
 			bad_area_nosemaphore(regs, error_code, address);
 			return;
 		}
 retry:
-		down_read(&mm->mmap_sem);
+		down_read_state(&mm->mmap_sem, &eliding);
 	} else {
 		/*
 		 * The above down_read_trylock() might have succeeded in
@@ -1136,6 +1142,8 @@ retry:
 		 */
 		might_sleep();
 	}
+	if (eliding)
+		flags |= FAULT_FLAG_ELIDING;
 
 	vma = find_vma(mm, address);
 	if (unlikely(!vma)) {
@@ -1227,7 +1235,7 @@ good_area:
 
 	check_v8086_mode(regs, address, tsk);
 
-	up_read(&mm->mmap_sem);
+	up_read_state(&mm->mmap_sem, eliding);
 }
 
 dotraplinkage void __kprobes
