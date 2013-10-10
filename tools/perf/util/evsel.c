d912c8ca2eca22566ea428b63a433b0127f7f753
Adrian Hunter <adrian.hunter@intel.com>
2013-10-10 15:22:23 +0300

    perf evsel: Add 'no_aux_samples' option
    Add an option to prevent additional
    samples being added to a selected
    event by perf_evsel__config().
    
    This is needed when using the sched_switch
    tracepoint to follow object code execution.
    Since sched_switch will be used only for
    switch information, additional sampling is
    wasteful.
    
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>

--- tools/perf/util/evsel.c
+++ tools/perf/util/evsel.c
@@ -595,7 +595,7 @@ void perf_evsel__config(struct perf_evsel *evsel, struct record_opts *opts)
 		attr->mmap_data = track;
 	}
 
-	if (opts->call_graph) {
+	if (opts->call_graph && !evsel->no_aux_samples) {
 		perf_evsel__set_sample_bit(evsel, CALLCHAIN);
 
 		if (opts->call_graph == CALLCHAIN_DWARF) {
@@ -618,7 +618,7 @@ void perf_evsel__config(struct perf_evsel *evsel, struct record_opts *opts)
 	     target__has_cpu(&opts->target) || per_cpu))
 		perf_evsel__set_sample_bit(evsel, TIME);
 
-	if (opts->raw_samples) {
+	if (opts->raw_samples && !evsel->no_aux_samples) {
 		perf_evsel__set_sample_bit(evsel, TIME);
 		perf_evsel__set_sample_bit(evsel, RAW);
 		perf_evsel__set_sample_bit(evsel, CPU);
@@ -631,7 +631,7 @@ void perf_evsel__config(struct perf_evsel *evsel, struct record_opts *opts)
 		attr->watermark = 0;
 		attr->wakeup_events = 1;
 	}
-	if (opts->branch_stack) {
+	if (opts->branch_stack && !evsel->no_aux_samples) {
 		perf_evsel__set_sample_bit(evsel, BRANCH_STACK);
 		attr->branch_sample_type = opts->branch_stack;
 	}
