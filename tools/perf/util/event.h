8627f439d7a72c302b9f14323af79e72ac2a4b5d
Adrian Hunter <adrian.hunter@intel.com>
2013-10-24 11:25:25 +0300

    perf tools: Add a user event for Instruction Tracing errors
    Errors encountered when decoding an Instruction
    Trace need to be reported to the user. However
    the "user" might be a script or another tool,
    so provide a new user event to capture those
    errors.
    
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>

--- tools/perf/util/event.h
+++ tools/perf/util/event.h
@@ -162,6 +162,7 @@ enum perf_user_event_type { /* above any possible kernel type */
 	PERF_RECORD_ID_INDEX			= 69,
 	PERF_RECORD_ITRACE_INFO			= 70,
 	PERF_RECORD_ITRACE			= 71,
+	PERF_RECORD_ITRACE_ERROR		= 72,
 	PERF_RECORD_HEADER_MAX
 };
 
@@ -224,6 +225,20 @@ struct itrace_lost_event {
 	u64 offset;
 };
 
+#define MAX_ITRACE_ERROR_MSG 64
+
+struct itrace_error_event {
+	struct perf_event_header header;
+	u32 type;
+	u32 code;
+	u32 cpu;
+	u32 pid;
+	u32 tid;
+	u32 reserved__; /* For alignment */
+	u64 ip;
+	char msg[MAX_ITRACE_ERROR_MSG];
+};
+
 union perf_event {
 	struct perf_event_header	header;
 	struct mmap_event		mmap;
@@ -242,6 +257,7 @@ union perf_event {
 	struct itrace_lost_event	itrace_lost;
 	struct itrace_info_event	itrace_info;
 	struct itrace_event		itrace;
+	struct itrace_error_event	itrace_error;
 };
 
 void perf_event__print_totals(void);
--- tools/perf/util/tool.h
+++ tools/perf/util/tool.h
@@ -47,7 +47,8 @@ struct perf_tool {
 	event_op2	finished_round,
 			build_id,
 			id_index,
-			itrace_info;
+			itrace_info,
+			itrace_error;
 	event_op3	itrace;
 	bool		ordered_samples;
 	bool		ordering_requires_timestamps;
