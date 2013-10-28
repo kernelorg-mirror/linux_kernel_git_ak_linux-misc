b2df8ecee843a971b9542d22a13e16958477c697
Adrian Hunter <adrian.hunter@intel.com>
2013-10-28 12:44:21 +0200

    perf tools: Take Intel PT into use
    To record an Instruction Trace, the weak function
    itrace_record__init() must be implemented.
    
    Equally to decode an Instruction Trace, the
    Instruction Tracing type must be added to the
    perf_event__process_itrace_info() function.
    
    This patch makes those two changes plus hooks
    up default config for the intel_pt PMU.  Also
    some brief documentation is provided for
    using the tools with intel_pt.
    
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>

--- tools/perf/util/itrace.c
+++ tools/perf/util/itrace.c
@@ -42,6 +42,8 @@
 #include "debug.h"
 #include "parse-options.h"
 
+#include "intel-pt.h"
+
 int itrace_mmap__mmap(struct itrace_mmap *mm, struct itrace_mmap_params *mp,
 		      int fd)
 {
@@ -975,9 +977,9 @@ static bool itrace__dont_decode(struct perf_session *session)
 	       session->itrace_synth_opts->dont_decode;
 }
 
-int perf_event__process_itrace_info(struct perf_tool *tool __maybe_unused,
+int perf_event__process_itrace_info(struct perf_tool *tool,
 				    union perf_event *event,
-				    struct perf_session *session __maybe_unused)
+				    struct perf_session *session)
 {
 	enum itrace_type type = event->itrace_info.type;
 
@@ -989,6 +991,7 @@ int perf_event__process_itrace_info(struct perf_tool *tool __maybe_unused,
 
 	switch (type) {
 	case PERF_ITRACE_INTEL_PT:
+		return intel_pt_process_itrace_info(tool, event, session);
 	case PERF_ITRACE_UNKNOWN:
 	default:
 		return -EINVAL;
