f69df22a54c355220cfac7eb8c8a63055c2ddcfd
Andi Kleen <ak@linux.intel.com>
2014-04-23 13:26:20 -0700

    x86: Add support for rd/wr fs/gs base
    Introduction:
    
    IvyBridge added four new instructions to directly write the fs and gs
    64bit base registers. Previously this had to be done with a system
    call to write to MSRs. The main use case is fast user space threading
    and switching the fs/gs registers quickly there. Another use
    case is having (relatively) cheap access to a new address
    register per thread.
    
    The instructions are opt-in and have to be explicitely enabled
    by the OS.
    
    For more details on how to use the instructions see
    Documentation/x86/fsgs.txt added in a followon patch.
    
    Paranoid exception path changes:
    ===============================
    
    The paranoid entry/exit code is used for any NMI like
    exception.
    
    Previously Linux couldn't support the new instructions
    because the paranoid entry code relied on the gs base never being
    negative outside the kernel to decide when to use swaps. It would
    check the gs MSR value and assume it was already running in
    kernel if negative.
    
    To get rid of this assumption we have to revamp the paranoid exception
    path to not rely on this. We can use the new instructions
    to get (relatively) quick access to the GS value, and use
    it directly.
    
    This is also significantly faster than a MSR read, so will speed
    NMIs (critical for profiling)
    
    The kernel gs for the paranoid path is now stored at the
    bottom of the IST stack (so that it can be derived from RSP).
    
    The original patch compared the gs with the kernel gs and
    assumed that if it was identical, swapgs was not needed
    (and no user space processing was needed). This
    was nice and simple and didn't need a lot of changes.
    
    But this had the side effect that if a user process set its
    GS to the same as the kernel it may lose rescheduling
    checks (so a racing reschedule IPI would have been
    only acted upon the next non paranoid interrupt)
    
    This version now switches to full save/restore of the GS.
    This requires quite some changes in the paranoid path.
    Unfortunately I didn't come up with a simpler scheme.
    
    When swapgs used to be needed, but we have the new
    instructions, we restore original GS value in the exit
    path.
    
    Context switch changes:
    ======================
    
    Then after these changes we need to also use the new instructions
    to save/restore fs and gs, so that the new values set by the
    users won't disappear.  This is also significantly
    faster for the case when the 64bit base has to be switched
    (that is when GS is larger than 4GB), as we can replace
    the slow MSR write with a faster wr[fg]sbase execution.
    
    This is in term enables fast switching when there are
    enough threads that their TLS segment does not fit below 4GB
    (or with some newer systems which don't properly hint the
    stack limit), or alternatively programs that use fs as an additional base
    register will not get a sigificant context switch penalty.
    
    It is all done in a single patch because there was no
    simple way to do it in pieces without having crash
    holes inbetween.
    
    v2: Change to save/restore GS instead of using swapgs
    based on the value. Large scale changes.
    v3: Fix wrong flag initialization in fallback path.
    Thanks 0day!
    v4: Make swapgs code paths kprobes safe.
    Port to new base line code which now switches indexes.
    v5: Port to new kernel which avoids paranoid entry for ring 3.
    Removed some code that handled this previously.
    v6: Remove obsolete code. Use macro for ALTERNATIVE. Use
    ALTERNATIVE for exit path, eliminating the DO_RESTORE_G15 flag.
    Various cleanups. Improve description.
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- arch/x86/kernel/process_64.c
+++ arch/x86/kernel/process_64.c
@@ -49,6 +49,7 @@
 #include <asm/syscalls.h>
 #include <asm/debugreg.h>
 #include <asm/switch_to.h>
+#include <asm/fsgs.h>
 
 asmlinkage extern void ret_from_fork(void);
 
@@ -261,6 +262,27 @@ void start_thread_ia32(struct pt_regs *regs, u32 new_ip, u32 new_sp)
 }
 #endif
 
+/* Out of line to be protected from kprobes. */
+
+/* Interrupts are disabled here. */
+static noinline __kprobes void switch_gs(unsigned long gs)
+{
+	swapgs();
+	wrgsbase(gs);
+	swapgs();
+}
+
+/* Interrupts are disabled here. */
+static noinline __kprobes unsigned long read_user_gs(void)
+{
+	unsigned long gs;
+
+	swapgs();
+	gs = rdgsbase();
+	swapgs();
+	return gs;
+}
+
 /*
  *	switch_to(x,y) should switch tasks from x to y.
  *
@@ -293,6 +315,10 @@ __switch_to(struct task_struct *prev_p, struct task_struct *next_p)
 	 */
 	savesegment(fs, fsindex);
 	savesegment(gs, gsindex);
+	if (static_cpu_has(X86_FEATURE_FSGSBASE)) {
+		prev->fs = rdfsbase();
+		prev->gs = read_user_gs();
+	}
 
 	/*
 	 * Load TLS before restoring any segments so that segment loads
@@ -381,8 +407,12 @@ __switch_to(struct task_struct *prev_p, struct task_struct *next_p)
 		if (fsindex)
 			prev->fs = 0;
 	}
-	if (next->fs)
-		wrmsrl(MSR_FS_BASE, next->fs);
+	if (next->fs) {
+		if (static_cpu_has(X86_FEATURE_FSGSBASE))
+			wrfsbase(next->fs);
+		else
+			wrmsrl(MSR_FS_BASE, next->fs);
+	}
 	prev->fsindex = fsindex;
 
 	if (unlikely(gsindex | next->gsindex | prev->gs)) {
@@ -392,8 +422,12 @@ __switch_to(struct task_struct *prev_p, struct task_struct *next_p)
 		if (gsindex)
 			prev->gs = 0;
 	}
-	if (next->gs)
-		wrmsrl(MSR_KERNEL_GS_BASE, next->gs);
+	if (next->gs) {
+		if (static_cpu_has(X86_FEATURE_FSGSBASE))
+			switch_gs(next->gs);
+		else
+			wrmsrl(MSR_KERNEL_GS_BASE, next->gs);
+	}
 	prev->gsindex = gsindex;
 
 	switch_fpu_finish(next_p, fpu);
