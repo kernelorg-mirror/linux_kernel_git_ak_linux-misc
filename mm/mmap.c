97aabae0f016cbbec5aeeb94a83980665276e018
Andi Kleen <ak@linux.intel.com>
2014-07-08 11:15:07 -0700

    tsx, mm: Avoid mmap sem write check for CONFIG_DEBUG_VM with lock elision
    Avoid trying to get the mmap sem for debugging when a lock is elided.
    This causes unnecessary aborts, for example in brk()
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- mm/mmap.c
+++ mm/mmap.c
@@ -2606,7 +2606,7 @@ SYSCALL_DEFINE2(munmap, unsigned long, addr, size_t, len)
 static inline void verify_mm_writelocked(struct mm_struct *mm)
 {
 #ifdef CONFIG_DEBUG_VM
-	if (unlikely(down_read_trylock(&mm->mmap_sem))) {
+	if (!_xtest() && unlikely(down_read_trylock(&mm->mmap_sem))) {
 		WARN_ON(1);
 		up_read(&mm->mmap_sem);
 	}
