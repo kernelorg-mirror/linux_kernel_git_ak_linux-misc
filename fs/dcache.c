9de44e03a94d7d660937e28bd7b5ffbe524af6c8
Andi Kleen <ak@linux.intel.com>
2014-07-14 13:47:40 -0700

    vfs: Disable lock elision for d_alloc
    The parent d_lock in d_alloc can have a high abort rate in
    some workload that access the same directory. The child list
    cache line is hot and causes a lot of conflicts.
    
    Disable lock elision for d_alloc.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- fs/dcache.c
+++ fs/dcache.c
@@ -1496,7 +1496,7 @@ struct dentry *d_alloc(struct dentry * parent, const struct qstr *name)
 	struct dentry *dentry = __d_alloc(parent->d_sb, name);
 	if (!dentry)
 		return NULL;
-
+	disable_txn();
 	spin_lock(&parent->d_lock);
 	/*
 	 * don't need child lock because it is not subject
@@ -1506,7 +1506,7 @@ struct dentry *d_alloc(struct dentry * parent, const struct qstr *name)
 	dentry->d_parent = parent;
 	list_add(&dentry->d_u.d_child, &parent->d_subdirs);
 	spin_unlock(&parent->d_lock);
-
+	reenable_txn();
 	return dentry;
 }
 EXPORT_SYMBOL(d_alloc);
