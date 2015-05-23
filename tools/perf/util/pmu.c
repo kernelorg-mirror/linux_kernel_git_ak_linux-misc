1256476d4323ee58b97d0b541fce9d01c852b9ea
Andi Kleen <ak@linux.intel.com>
2015-05-22 22:33:38 -0400

    perf, tools: Query terminal width and use in perf list
    Automatically adapt the now wider and word wrapped perf list
    output to wider terminals. This requires querying the terminal
    before the auto pager takes over, and exporting this
    information from the pager subsystem.
    
    Acked-by: Namhyung Kim <namhyung@kernel.org>
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- tools/perf/util/pmu.c
+++ tools/perf/util/pmu.c
@@ -13,6 +13,7 @@
 #include "pmu-events/pmu-events.h"
 #include "cpumap.h"
 #include "header.h"
+#include "cache.h"
 
 struct perf_pmu_format {
 	char *name;
@@ -980,7 +981,7 @@ void print_pmu_events(const char *event_glob, bool name_only)
 	int len, j;
 	struct pair *aliases;
 	int numdesc = 0;
-	int columns = 78;
+	int columns = pager_get_columns();
 
 	pmu = NULL;
 	len = 0;
