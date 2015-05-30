78138b6d6ba05e868489663b13333bf591152a32
Andi Kleen <ak@linux.intel.com>
2015-05-30 08:30:17 -0700

    perf, tools: Add support for event list topics
    Add support to group the output of perf list by the Topic field
    in the JSON file.
    
    Example output:
    
    % perf list
    ...
    Cache:
      l1d.replacement
           [L1D data line replacements]
      l1d_pend_miss.pending
           [L1D miss oustandings duration in cycles]
      l1d_pend_miss.pending_cycles
           [Cycles with L1D load Misses outstanding]
      l2_l1d_wb_rqsts.all
           [Not rejected writebacks from L1D to L2 cache lines in any state]
      l2_l1d_wb_rqsts.hit_e
           [Not rejected writebacks from L1D to L2 cache lines in E state]
      l2_l1d_wb_rqsts.hit_m
           [Not rejected writebacks from L1D to L2 cache lines in M state]
    
    ...
    Pipeline:
      arith.fpu_div
           [Divide operations executed]
      arith.fpu_div_active
           [Cycles when divider is busy executing divide operations]
      baclears.any
           [Counts the total number when the front end is resteered, mainly when the BPU cannot provide a correct prediction and this is corrected by
            other branch handling mechanisms at the front end]
      br_inst_exec.all_branches
           [Speculative and retired branches]
      br_inst_exec.all_conditional
           [Speculative and retired macro-conditional branches]
      br_inst_exec.all_direct_jmp
           [Speculative and retired macro-unconditional branches excluding calls and indirects]
      br_inst_exec.all_direct_near_call
           [Speculative and retired direct near calls]
      br_inst_exec.all_indirect_jump_non_call_ret
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- tools/perf/pmu-events/jevents.c
+++ tools/perf/pmu-events/jevents.c
@@ -202,7 +202,7 @@ static void print_events_table_prefix(FILE *fp, const char *tblname)
 }
 
 static int print_events_table_entry(void *data, char *name, char *event,
-				    char *desc, char *long_desc)
+				    char *desc, char *long_desc, char *topic)
 {
 	FILE *outfp = data;
 	/*
@@ -215,7 +215,9 @@ static int print_events_table_entry(void *data, char *name, char *event,
 	fprintf(outfp, "\t.event = \"%s\",\n", event);
 	fprintf(outfp, "\t.desc = \"%s\",\n", desc);
 	if (long_desc && long_desc[0])
-		fprintf(outfp, "\t.long_desc = \"%s\"", long_desc);
+		fprintf(outfp, "\t.long_desc = \"%s\",\n", long_desc);
+	if (topic)
+		fprintf(outfp, "\t.topic = \"%s\",\n", topic);
 	fprintf(outfp, "},\n");
 
 	return 0;
@@ -235,7 +237,8 @@ static void print_events_table_suffix(FILE *outfp)
 
 /* Call func with each event in the json file */
 int json_events(const char *fn,
-	  int (*func)(void *data, char *name, char *event, char *desc, char *long_desc),
+	  int (*func)(void *data, char *name, char *event, char *desc,
+		      char *long_desc, char *topic),
 	  void *data)
 {
 	int err = -EIO;
@@ -254,7 +257,7 @@ int json_events(const char *fn,
 	tok = tokens + 1;
 	for (i = 0; i < tokens->size; i++) {
 		char *event = NULL, *desc = NULL, *name = NULL, *long_desc = NULL;
-		char *extra_desc = NULL;
+		char *extra_desc = NULL, *topic = NULL;
 		struct msrmap *msr = NULL;
 		jsmntok_t *msrval = NULL;
 		jsmntok_t *precise = NULL;
@@ -293,6 +296,8 @@ int json_events(const char *fn,
 				   !json_streq(map, val, "null")) {
 				addfield(map, &extra_desc, ". ",
 					" Spec update: ", val);
+			} else if (json_streq(map, field, "Topic")) {
+				addfield(map, &topic, "", "", val);
 			} else if (json_streq(map, field, "Data_LA") && nz) {
 				addfield(map, &extra_desc, ". ",
 					" Supports address when precise",
@@ -315,12 +320,13 @@ int json_events(const char *fn,
 		if (msr != NULL)
 			addfield(map, &event, ",", msr->pname, msrval);
 		fixname(name);
-		err = func(data, name, event, desc, long_desc);
+		err = func(data, name, event, desc, long_desc, topic);
 		free(event);
 		free(desc);
 		free(name);
 		free(extra_desc);
 		free(long_desc);
+		free(topic);
 		if (err)
 			break;
 		tok += j;
