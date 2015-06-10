275dee0af2fe74822e09fbdeb42e137c445671ae
Andi Kleen <ak@linux.intel.com>
2015-06-10 16:01:11 -0700

    perf, tools: Factor out scale conversion code
    Move the scale factor parsing code to an own function
    to reuse it in an upcoming patch.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- tools/perf/util/pmu.c
+++ tools/perf/util/pmu.c
@@ -94,6 +94,40 @@ static int pmu_format(const char *name, struct list_head *format)
 	return 0;
 }
 
+static double convert_scale(const char *scale, char **end)
+{
+	char *lc;
+	double sval;
+
+	/*
+	 * save current locale
+	 */
+	lc = setlocale(LC_NUMERIC, NULL);
+
+	/*
+	 * The lc string may be allocated in static storage,
+	 * so get a dynamic copy to make it survive setlocale
+	 * call below.
+	 */
+	lc = strdup(lc);
+	if (!lc)
+		return 1.0;
+
+	/*
+	 * force to C locale to ensure kernel
+	 * scale string is converted correctly.
+	 * kernel uses default C locale.
+	 */
+	setlocale(LC_NUMERIC, "C");
+
+	sval = strtod(scale, end);
+
+	/* restore locale */
+	setlocale(LC_NUMERIC, lc);
+	free(lc);
+	return sval;
+}
+
 static int perf_pmu__parse_scale(struct perf_pmu_alias *alias, char *dir, char *name)
 {
 	struct stat st;
@@ -101,7 +135,6 @@ static int perf_pmu__parse_scale(struct perf_pmu_alias *alias, char *dir, char *
 	char scale[128];
 	int fd, ret = -1;
 	char path[PATH_MAX];
-	char *lc;
 
 	snprintf(path, PATH_MAX, "%s/%s.scale", dir, name);
 
@@ -121,36 +154,7 @@ static int perf_pmu__parse_scale(struct perf_pmu_alias *alias, char *dir, char *
 	else
 		scale[sret] = '\0';
 
-	/*
-	 * save current locale
-	 */
-	lc = setlocale(LC_NUMERIC, NULL);
-
-	/*
-	 * The lc string may be allocated in static storage,
-	 * so get a dynamic copy to make it survive setlocale
-	 * call below.
-	 */
-	lc = strdup(lc);
-	if (!lc) {
-		ret = -ENOMEM;
-		goto error;
-	}
-
-	/*
-	 * force to C locale to ensure kernel
-	 * scale string is converted correctly.
-	 * kernel uses default C locale.
-	 */
-	setlocale(LC_NUMERIC, "C");
-
-	alias->scale = strtod(scale, NULL);
-
-	/* restore locale */
-	setlocale(LC_NUMERIC, lc);
-
-	free(lc);
-
+	alias->scale = convert_scale(scale, NULL);
 	ret = 0;
 error:
 	close(fd);
