33be9b2d6a1907e15ed3ef5af5e7f93a01e06629
Andi Kleen <ak@linux.intel.com>
2015-07-31 17:48:51 -0700

    perf, tools: Support metrics in --per-core/socket mode
    Enable metrics printing in --per-core / --per-socket mode. We need
    to save the shadow metrics in a unique place. Always use the first
    CPU in the aggregation. Then use the same CPU to retrieve the
    shadow value later.
    
    Example output:
    
    % perf stat --per-core -a ./BC1s
    
     Performance counter stats for 'system wide':
    
    S0-C0           2        2966.020381      task-clock (msec)         #    2.004 CPUs utilized            (100.00%)
    S0-C0           2                 49      context-switches          #    0.017 K/sec                    (100.00%)
    S0-C0           2                  4      cpu-migrations            #    0.001 K/sec                    (100.00%)
    S0-C0           2                467      page-faults               #    0.157 K/sec
    S0-C0           2      4,599,061,773      cycles                    #    1.551 GHz                      (100.00%)
    S0-C0           2      9,755,886,883      instructions              #    2.12  insn per cycle           (100.00%)
    S0-C0           2      1,906,272,125      branches                  #  642.704 M/sec                    (100.00%)
    S0-C0           2         81,180,867      branch-misses             #    4.26% of all branches
    S0-C1           2        2965.995373      task-clock (msec)         #    2.003 CPUs utilized            (100.00%)
    S0-C1           2                 62      context-switches          #    0.021 K/sec                    (100.00%)
    S0-C1           2                  8      cpu-migrations            #    0.003 K/sec                    (100.00%)
    S0-C1           2                281      page-faults               #    0.095 K/sec
    S0-C1           2          6,347,290      cycles                    #    0.002 GHz                      (100.00%)
    S0-C1           2          4,654,156      instructions              #    0.73  insn per cycle           (100.00%)
    S0-C1           2            947,121      branches                  #    0.319 M/sec                    (100.00%)
    S0-C1           2             37,322      branch-misses             #    3.94% of all branches
    
           1.480409747 seconds time elapsed
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- tools/perf/builtin-stat.c
+++ tools/perf/builtin-stat.c
@@ -731,6 +731,22 @@ static void nsec_printout(int id, int nr, struct perf_evsel *evsel, double avg)
 		fprintf(output, "%s%s", csv_sep, evsel->cgrp->name);
 }
 
+static int first_shadow_cpu(struct perf_evsel *evsel, int id)
+{
+	int i;
+
+	if (aggr_get_id == NULL)
+		return 0;
+
+	for (i = 0; i < perf_evsel__nr_cpus(evsel); i++) {
+		int cpu2 = perf_evsel__cpus(evsel)->map[i];
+
+		if (aggr_get_id(evsel_list->cpus, cpu2) == id)
+			return cpu2;
+	}
+	return 0;
+}
+
 static void abs_printout(int id, int nr, struct perf_evsel *evsel, double avg)
 {
 	double sc =  evsel->scale;
@@ -800,8 +816,7 @@ static void printout(int id, int nr, struct perf_evsel *counter, double uval,
 		abs_printout(id, nr, counter, uval);
 
 	perf_stat__print_shadow_stats(counter, uval,
-				      aggr_mode == AGGR_GLOBAL ? 0 :
-				      cpu_map__id_to_cpu(id),
+				      first_shadow_cpu(counter, id),
 				      pm,
 				      nl,
 				      &os);
@@ -810,6 +825,30 @@ static void printout(int id, int nr, struct perf_evsel *counter, double uval,
 		print_noise(counter, noise);
 		if (run != ena)
 			fprintf(output, "  (%.2f%%)", 100.0 * run / ena);
+	}
+}
+
+static void aggr_update_shadow(void)
+{
+	int cpu, cpu2, s2, id, s;
+	u64 val;
+	struct perf_evsel *counter;
+
+	for (s = 0; s < aggr_map->nr; s++) {
+		id = aggr_map->map[s];
+		evlist__for_each(evsel_list, counter) {
+			val = 0;
+			for (cpu = 0; cpu < perf_evsel__nr_cpus(counter); cpu++) {
+				cpu2 = perf_evsel__cpus(counter)->map[cpu];
+				s2 = aggr_get_id(evsel_list->cpus, cpu2);
+				if (s2 != id)
+					continue;
+				val += perf_counts(counter->counts, cpu, 0)->val;
+			}
+			val = scale_val(counter, val);
+			perf_stat__update_shadow_stats(counter, &val,
+						       first_shadow_cpu(counter, id));
+		}
 	}
 }
 
@@ -822,6 +861,8 @@ static void print_aggr(char *prefix)
 
 	if (!(aggr_map || aggr_get_id))
 		return;
+
+	aggr_update_shadow();
 
 	for (s = 0; s < aggr_map->nr; s++) {
 		id = aggr_map->map[s];
