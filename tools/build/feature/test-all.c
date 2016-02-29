f7ec5be0e75b0b22d1dc3fdba30ea80d6f62931a
Andi Kleen <ak@linux.intel.com>
2016-02-29 14:37:47 -0800

    perf, tools: Add probing for udev86
    Add autoprobing for the udev86 disassembler library.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- tools/build/feature/test-all.c
+++ tools/build/feature/test-all.c
@@ -137,6 +137,10 @@
 # include "test-libcrypto.c"
 #undef main
 
+#define main main_test_udis86
+#  include "test-udis86.c"
+#endif
+
 int main(int argc, char *argv[])
 {
 	main_test_libpython();
@@ -168,6 +172,7 @@ int main(int argc, char *argv[])
 	main_test_get_cpuid();
 	main_test_bpf();
 	main_test_libcrypto();
+	main_test_udis86();
 
 	return 0;
 }
