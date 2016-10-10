56e94e51a66b185377e985b9431bc149eefabebc
Andi Kleen <ak@linux.intel.com>
2016-10-10 12:52:39 -0700

    perf, tools: Only print Using CPUID message once
    With uncore event aliases which are duplicated over multiple PMUs
    the "Using CPUID" message with -v could be printed many times.
    Only print it once.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- tools/perf/util/pmu.c
+++ tools/perf/util/pmu.c
@@ -508,6 +508,7 @@ static void pmu_add_cpu_aliases(struct list_head *head)
 	struct pmu_events_map *map;
 	struct pmu_event *pe;
 	char *cpuid;
+	static bool printed;
 
 	cpuid = getenv("PERF_CPUID");
 	if (cpuid)
@@ -517,7 +518,10 @@ static void pmu_add_cpu_aliases(struct list_head *head)
 	if (!cpuid)
 		return;
 
-	pr_debug("Using CPUID %s\n", cpuid);
+	if (!printed) {
+		pr_debug("Using CPUID %s\n", cpuid);
+		printed = true;
+	}
 
 	i = 0;
 	while (1) {
