a957080132981aea0126ccbc33935946b45e7b2e
Andi Kleen <ak@linux.intel.com>
2016-12-22 20:04:22 -0800

    perf, tools: Add disassembler for x86 using the XED library
    Add a generic disassembler function for x86 using the XED library,
    and a fallback function for architectures that don't implement one.
    Other architectures can implement their own disassembler functions.
    
    The previous version of this patch used udis86, but was
    rejected because udis86 was unmaintained and a runtime dependency.
    Using the recently released xed avoids both of these problems:
    - XED is well maintained, uptodate, and used by many Intel tools
    - XED is linked statically so there is no runtime dependency.
    
    The XED library can be downloaded from http://github.com/intelxed/xed
    
    v2: Clean up includes.
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

---
+++ tools/perf/util/dis.h
@@ -0,0 +1,20 @@
+#ifndef DIS_H
+#define DIS_H 1
+
+#define MAXINSN 15
+
+struct perf_dis {
+	/* Initialized by callers: */
+	struct thread *thread;
+	u8 cpumode;
+	int cpu;
+	bool is64bit;
+	/* Temporary */
+	char out[256];
+};
+
+char *disas_inst(struct perf_dis *x, uint64_t ip, u8 *inbuf, int inlen,
+		 int *lenp);
+
+
+#endif
