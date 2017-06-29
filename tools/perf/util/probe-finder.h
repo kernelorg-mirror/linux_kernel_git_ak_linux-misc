7b2d9d1fd771cf517849df9a4d5c8668eb5bb9dc
Andi Kleen <ak@linux.intel.com>
2017-06-29 10:57:46 -0700

    perf, tools: Store variable name and register for dwarf variable lists
    Extend the strlist returned by debuginfo__find_available_vars_at to also
    directly include the variable name and the location of the resolved
    variables in each node. This makes it easier to use for callers that parse the output
    instead of just printing it.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- tools/perf/util/probe-finder.h
+++ tools/perf/util/probe-finder.h
@@ -4,6 +4,7 @@
 
 #include <stdbool.h>
 #include "intlist.h"
+#include "strlist.h"
 #include "probe-event.h"
 #include <linux/ctype.h>
 
@@ -116,6 +117,12 @@ struct line_finder {
 	Dwarf_Die		cu_die;		/* Current CU */
 	Dwarf_Die		sp_die;
 	int			found;
+};
+
+struct variable_node {
+	struct str_node		snode;
+	char			value[64];
+	char			name[256];
 };
 
 #endif /* HAVE_DWARF_SUPPORT */
