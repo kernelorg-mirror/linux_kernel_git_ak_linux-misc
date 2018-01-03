0c335657c2673cb709db33287e10ae1aca0d97c4
Andi Kleen <ak@linux.intel.com>
2018-01-03 12:33:02 -0800

    x86/retpoline/crypto: Convert xen assembler indirect jumps
    Convert all indirect jumps in xen inline assembler code to use
    non speculative sequences.
    
    Based on code from David Woodhouse and Tim Chen
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- arch/x86/include/asm/xen/hypercall.h
+++ arch/x86/include/asm/xen/hypercall.h
@@ -44,6 +44,7 @@
 #include <asm/page.h>
 #include <asm/pgtable.h>
 #include <asm/smap.h>
+#include <asm/jump-asm.h>
 
 #include <xen/interface/xen.h>
 #include <xen/interface/sched.h>
@@ -217,7 +218,7 @@ privcmd_call(unsigned call,
 	__HYPERCALL_5ARG(a1, a2, a3, a4, a5);
 
 	stac();
-	asm volatile("call *%[call]"
+	asm volatile(NOSPEC_CALL("%[call]")
 		     : __HYPERCALL_5PARAM
 		     : [call] "a" (&hypercall_page[call])
 		     : __HYPERCALL_CLOBBER5);
