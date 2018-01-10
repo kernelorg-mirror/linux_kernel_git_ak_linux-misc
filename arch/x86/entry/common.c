679a4a29537eaa01b9af16e1d7c3a7983b7b9468
Andi Kleen <ak@linux.intel.com>
2018-01-10 13:30:24 -0800

    x86/entry/clearregs: Force rt_sigreturn into slow path
    rt_sigreturn is the only system that needs to restore all
    registers.  So force it to go through the slow path,
    so that we don't need the pt_regs stub anymore.
    
    Force it by setting a new dummy work bit.
    
    iopl will work even in the fast path.
    execve works with the fast path too. It will leak
    the original R11 into the next process, but that seems
    acceptable.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- arch/x86/entry/common.c
+++ arch/x86/entry/common.c
@@ -210,6 +210,7 @@ __visible inline void prepare_exit_to_usermode(struct pt_regs *regs)
 	 */
 	current->thread.status &= ~(TS_COMPAT|TS_I386_REGS_POKED);
 #endif
+	current->thread.status &= ~TIF_FULL_RESTORE;
 
 	user_enter_irqoff();
 }
