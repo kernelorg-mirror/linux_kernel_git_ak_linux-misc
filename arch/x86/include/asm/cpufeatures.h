bc3eee43b4873b465c15cb5a3c620aa1f575cf20
Andi Kleen <ak@linux.intel.com>
2018-01-11 16:53:28 -0800

    x86/retpoline: Add feature bit for additional return buffer protections
    On Skylake we want additional protections against spectre_v2
    over the normal RETPOLINE against underflowing return buffers.
    
    On return buffer underflow the CPU could fall back to
    the poisoned indirect branch predictor.
    
    This patch adds a feature bit, only enabled on Skylake,
    to enable additional return buffer underflow protections
    
    This will be used in the next patches
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- arch/x86/include/asm/cpufeatures.h
+++ arch/x86/include/asm/cpufeatures.h
@@ -213,6 +213,8 @@
 #define X86_FEATURE_USE_IBPB		( 7*32+21) /* "" Indirect Branch Prediction Barrier enabled */
 #define X86_FEATURE_USE_IBRS_FW		( 7*32+22) /* "" Use IBRS during runtime firmware calls */
 
+#define X86_FEATURE_RSB_UNDERFLOW	( 7*32+22) /* Avoid return stack underflows */
+
 /* Virtualization flags: Linux defined, word 8 */
 #define X86_FEATURE_TPR_SHADOW		( 8*32+ 0) /* Intel TPR Shadow */
 #define X86_FEATURE_VNMI		( 8*32+ 1) /* Intel Virtual NMI */
