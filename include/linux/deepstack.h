fbb17ce72f1f2aa925bc44ec7070a43a3dfc08e0
Andi Kleen <ak@linux.intel.com>
2018-01-17 15:09:47 -0800

    fixup! x86: Add infrastructure to mark and check deep call chains

--- include/linux/deepstack.h
+++ include/linux/deepstack.h
@@ -24,4 +24,16 @@ static inline void end_deep_call_chain(void)
 #endif
 }
 
+/*
+ * Used for softirqs and special situations, where we always fill later
+ * elsewhere, but not using end_deep_call_chain(). Normal code
+ * should use start/end_deep_call_chain.
+ */
+static inline void mark_deep_call_chain(void)
+{
+#ifdef CONFIG_DEBUG_STACK_DEPTH
+	set_thread_flag(TIF_DEEP_STACK);
+#endif
+}
+
 #endif
