994387edbf9c571c6843a25dcdeb8043042cce7b
Andi Kleen <ak@linux.intel.com>
2018-02-08 10:29:43 -0800

    x86/deepchain: Implement return patching for core kernel
    To maintain a true call depth for functions we need both a function
    and an exit hook. The entry hook has been already handled earlier
    using ftrace's hook. This adds the infrastructure for a return
    hook, and also patches it in when deep chain mitigations are disabled.
    
    This relies on a patch for gcc which is currently available here, but will
    be submitted to gcc mainline.  The patch automatically adds a 5 byte
    nop to each return for each instrumented function, and also
    provides a __return_loc section with pointers to all these return nops.
    
    gcc 7 patch is available here
    https://github.com/andikleen/gcc/tree/instrument-return-gcc7-1
    
    I originally tried to do it without a compiler patch with various tricks
    (redefining return, modify return address on stack etc.), but all
    of this ran into various problems, so I ended up with the relatively
    simple and clean compiler based solution.
    
    We replace the 5 byte nop with a call to the return hook, which
    increments the call depth and checks for underflows. If an underflow
    happens it fills the return buffer.
    
    I collected cycle data for the return hook during a kernel build
    using timed LBR. The mean is around 7 cycles, but the vast majority
    is 1 or 2 cycles. The slower instances are likely when the call
    depth variable dropped out of the L1 cache. So it's quite cheap
    as long as the L1 cache stays hot.
    
        1.0000 -     1.0000 [  2157]: ∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎
        1.0000 -     2.0000 [  2873]: ∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎
        2.0000 -     3.0000 [   363]: ∎∎∎∎∎∎∎∎∎
        3.0000 -     4.0000 [   393]: ∎∎∎∎∎∎∎∎∎∎
        4.0000 -     5.0000 [   447]: ∎∎∎∎∎∎∎∎∎∎∎
        5.0000 -     6.0000 [   598]: ∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎
        6.0000 -     7.0000 [   592]: ∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎
        7.0000 -     8.0000 [   545]: ∎∎∎∎∎∎∎∎∎∎∎∎∎∎
        8.0000 -     9.0000 [   577]: ∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎
        9.0000 -    10.0000 [   552]: ∎∎∎∎∎∎∎∎∎∎∎∎∎∎
       10.0000 -    11.0000 [   892]: ∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎
       11.0000 -    12.0000 [   486]: ∎∎∎∎∎∎∎∎∎∎∎∎
       12.0000 -    13.0000 [   416]: ∎∎∎∎∎∎∎∎∎∎
       13.0000 -    14.0000 [   213]: ∎∎∎∎∎
       14.0000 -    15.0000 [   194]: ∎∎∎∎∎
       15.0000 -    20.0000 [   604]: ∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎
       20.0000 -    30.0000 [   285]: ∎∎∎∎∎∎∎
    
    The return patch nops add around 10k text size (+0.08%) on my kernel config
    
    13597624	5526172	11079680	30203476	1ccde54	vmlinux-before-return
    13607929	5836188	11079680	30523797	1d1c195	vmlinux-return
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- arch/x86/kernel/cpu/bugs.c
+++ arch/x86/kernel/cpu/bugs.c
@@ -12,6 +12,7 @@
 #include <linux/utsname.h>
 #include <linux/cpu.h>
 #include <linux/module.h>
+#include <linux/deepchain.h>
 
 #include <asm/nospec-branch.h>
 #include <asm/cmdline.h>
@@ -325,6 +326,7 @@ retpoline_auto:
 	if (is_skylake_era()) {
 		setup_force_cpu_cap(X86_FEATURE_RSB_UNDERFLOW);
 		pr_info("Enabling return buffer underflow protections");
+		deepchain_init();
 	}
 }
 
