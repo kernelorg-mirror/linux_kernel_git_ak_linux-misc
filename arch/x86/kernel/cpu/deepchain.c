44cb4366be809e4bc632b54d156e6153d13cbcb3
Andi Kleen <ak@linux.intel.com>
2018-02-08 10:29:43 -0800

    x86/deepchain: Implement return patching for core kernel
    To maintain a true call depth for functions we need both a function
    and an exit hook. The entry hook has been already handled earlier
    using ftrace's hook. This adds the infrastructure for a return
    hook, and also patches it in when deep chain mitigations are disabled.
    
    This relies on a patch for gcc which is currently available here, but will
    be submitted to gcc mainline.  The patch automatically adds a 5 byte
    nop to each return for each instrumented function, and also
    provides a __return_loc section with pointers to all these return nops.
    
    gcc 7 patch is available here
    https://github.com/andikleen/gcc/tree/instrument-return-gcc7-1
    
    I originally tried to do it without a compiler patch with various tricks
    (redefining return, modify return address on stack etc.), but all
    of this ran into various problems, so I ended up with the relatively
    simple and clean compiler based solution.
    
    We replace the 5 byte nop with a call to the return hook, which
    increments the call depth and checks for underflows. If an underflow
    happens it fills the return buffer.
    
    I collected cycle data for the return hook during a kernel build
    using timed LBR. The mean is around 7 cycles, but the vast majority
    is 1 or 2 cycles. The slower instances are likely when the call
    depth variable dropped out of the L1 cache. So it's quite cheap
    as long as the L1 cache stays hot.
    
        1.0000 -     1.0000 [  2157]: ∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎
        1.0000 -     2.0000 [  2873]: ∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎
        2.0000 -     3.0000 [   363]: ∎∎∎∎∎∎∎∎∎
        3.0000 -     4.0000 [   393]: ∎∎∎∎∎∎∎∎∎∎
        4.0000 -     5.0000 [   447]: ∎∎∎∎∎∎∎∎∎∎∎
        5.0000 -     6.0000 [   598]: ∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎
        6.0000 -     7.0000 [   592]: ∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎
        7.0000 -     8.0000 [   545]: ∎∎∎∎∎∎∎∎∎∎∎∎∎∎
        8.0000 -     9.0000 [   577]: ∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎
        9.0000 -    10.0000 [   552]: ∎∎∎∎∎∎∎∎∎∎∎∎∎∎
       10.0000 -    11.0000 [   892]: ∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎
       11.0000 -    12.0000 [   486]: ∎∎∎∎∎∎∎∎∎∎∎∎
       12.0000 -    13.0000 [   416]: ∎∎∎∎∎∎∎∎∎∎
       13.0000 -    14.0000 [   213]: ∎∎∎∎∎
       14.0000 -    15.0000 [   194]: ∎∎∎∎∎
       15.0000 -    20.0000 [   604]: ∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎
       20.0000 -    30.0000 [   285]: ∎∎∎∎∎∎∎
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

---
+++ arch/x86/kernel/cpu/deepchain.c
@@ -0,0 +1,63 @@
+/* Copyright 2018 Intel Corporation
+ * Author: Andi Kleen
+ *
+ * Support to maintain call chain depth counters for automatic
+ * return buffer stuffing to avoid speculation attacks on Skylake
+ * CPUs. We use ftrace hooks and custom return instrumentation
+ * to maintain a dynamic call depth counter.
+ *
+ * SPDX-License-Identifier: GPL-2.0
+ */
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/string.h>
+#include <linux/init.h>
+#include <linux/deepchain.h>
+#include <asm/sections.h>
+#include <asm/text-patching.h>
+#include <asm/nospec-branch.h>
+
+asm("nop5_insn:\n"
+    "   " _ASM_MK_NOP(P6_NOP5) "\n"
+    ASM_UNREACHABLE
+    ".size nop5_insn,.-nop5_insn\n");
+
+extern char nop5_insn[];
+
+__visible DEFINE_PER_CPU(int, __call_depth__) = CALL_DEPTH_INIT;
+
+EXPORT_PER_CPU_SYMBOL(__call_depth__);
+
+extern void __visible __return__(void);
+
+void deepchain_return_patch(unsigned long *entries, unsigned num)
+{
+	unsigned i;
+
+	pr_debug("deep-chain: patching %u returns\n", num);
+	for (i = 0; i < num; i++) {
+		char *insnp = (char *)entries[i];
+		char call[5];
+		int offset;
+
+		if (memcmp(insnp, nop5_insn, 5)) {
+			pr_warn("Unexpected return entry at %p: %02x %02x %02x %02x %02x\n",
+				insnp,
+				insnp[0],
+				insnp[1],
+				insnp[2],
+				insnp[3],
+				insnp[4]);
+			continue;
+		}
+		call[0] = 0xe9;	/* jmp */
+		offset = (unsigned long)__return__ - (unsigned long)insnp - 5;
+		memcpy(call + 1, &offset, 4);
+		memcpy(insnp, call, 5);
+	}
+}
+
+__init void deepchain_init(void)
+{
+	deepchain_return_patch(__start_return_loc, __end_return_loc - __start_return_loc);
+}
