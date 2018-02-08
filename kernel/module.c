6ff429f2dfac7c3f47b3e7c4ba5cfdd330be5859
Andi Kleen <ak@linux.intel.com>
2018-02-08 09:32:10 -0800

    x86/deepchain: Add module return patching for deepchain
    When loading a module patch the return nops. of the module with our
    return hook. For the entries ftrace already takes care of it.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- kernel/module.c
+++ kernel/module.c
@@ -64,6 +64,7 @@
 #include <linux/bsearch.h>
 #include <linux/dynamic_debug.h>
 #include <linux/audit.h>
+#include <linux/deepchain.h>
 #include <uapi/linux/module.h>
 #include "module-internal.h"
 
@@ -3129,6 +3130,11 @@ static int find_module_sections(struct module *mod, struct load_info *info)
 					     sizeof(*mod->ftrace_callsites),
 					     &mod->num_ftrace_callsites);
 #endif
+#ifdef CONFIG_DEEP_CHAIN
+	mod->return_sites = section_objs(info, "__return_loc",
+					     sizeof(*mod->return_sites),
+					     &mod->num_return_sites);
+#endif
 
 	mod->extable = section_objs(info, "__ex_table",
 				    sizeof(*mod->extable), &mod->num_exentries);
@@ -3740,6 +3746,8 @@ static int load_module(struct load_info *info, const char __user *uargs,
 
 	/* Ftrace init must be called in the MODULE_STATE_UNFORMED state */
 	ftrace_module_init(mod);
+
+	deepchain_return_patch(mod->return_sites, mod->num_return_sites);
 
 	/* Finally it's fully formed, ready to start executing. */
 	err = complete_formation(mod, info);
