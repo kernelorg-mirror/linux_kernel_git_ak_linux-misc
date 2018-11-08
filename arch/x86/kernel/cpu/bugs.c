45e504714969e4f58550bb310793d5aba909d272
Andi Kleen <ak@linux.intel.com>
2018-11-07 16:08:39 -0800

    x86/speculation/mds: Add basic bug infrastructure for MDS
    MDS is micro architectural data sampling, which is a side channel
    attack on internal buffers in Intel CPUs. They all have
    the same mitigations for single thread, so we lump them all
    together as a single MDS issue.
    
    This addresses CVE-2018-12126 CVE-2018-12130 CVE-2018-12127
    for the single threaded case.
    
    This patch adds the basic infrastructure to detect if the current
    CPU is affected by MDS, and if yes set the right BUG bits.
    
    We also provide a command line option "mds_disable" to disable
    any workarounds.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- arch/x86/kernel/cpu/bugs.c
+++ arch/x86/kernel/cpu/bugs.c
@@ -35,6 +35,7 @@
 static void __init spectre_v2_select_mitigation(void);
 static void __init ssb_select_mitigation(void);
 static void __init l1tf_select_mitigation(void);
+static void __init mds_select_mitigation(void);
 
 /* The base value of the SPEC_CTRL MSR that always has to be preserved. */
 u64 x86_spec_ctrl_base;
@@ -98,6 +99,8 @@ void __init check_bugs(void)
 	ssb_select_mitigation();
 
 	l1tf_select_mitigation();
+
+	mds_select_mitigation();
 
 #ifdef CONFIG_X86_32
 	/*
@@ -1040,6 +1043,13 @@ static int __init l1tf_cmdline(char *str)
 early_param("l1tf", l1tf_cmdline);
 
 #undef pr_fmt
+
+static void mds_select_mitigation(void)
+{
+	if (cmdline_find_option_bool(boot_command_line, "mds=off") ||
+	    !boot_cpu_has_bug(X86_BUG_MDS)) {
+		setup_clear_cpu_cap(X86_FEATURE_MB_CLEAR);
+}
 
 #ifdef CONFIG_SYSFS
 
