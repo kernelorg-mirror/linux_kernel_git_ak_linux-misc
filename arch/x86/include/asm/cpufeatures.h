bec0d3681fe131dd9275328e410dad95a2a70c5c
Andi Kleen <ak@linux.intel.com>
2018-11-07 16:08:39 -0800

    x86/speculation/mds: Add basic bug infrastructure for MDS
    MDS is micro architectural data sampling, which is a side channel
    attack on internal buffers in Intel CPUs.
    
    MDS consists of multiple sub-vulnerabilities:
    Microarchitectural Store Buffer Data Sampling (MSBDS) (CVE-2018-12126)
    Microarchitectual Fill Buffer Data Sampling (MFBDS) (CVE-2018-12130)
    Microarchitectual Load Port Data (MLPDS) (CVE-2018-12127),
    with the first leaking store data, and the second loads and sometimes
    store data, and the third load data.
    
    They all have the same mitigations for single thread, so we lump them all
    together as a single MDS issue.
    
    This patch adds the basic infrastructure to detect if the current
    CPU is affected by MDS, and if yes set the right BUG bits.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- arch/x86/include/asm/cpufeatures.h
+++ arch/x86/include/asm/cpufeatures.h
@@ -381,5 +381,6 @@
 #define X86_BUG_SPECTRE_V2		X86_BUG(16) /* CPU is affected by Spectre variant 2 attack with indirect branches */
 #define X86_BUG_SPEC_STORE_BYPASS	X86_BUG(17) /* CPU is affected by speculative store bypass attack */
 #define X86_BUG_L1TF			X86_BUG(18) /* CPU is affected by L1 Terminal Fault */
+#define X86_BUG_MDS			X86_BUG(19) /* CPU is affected by Microarchitectural data sampling */
 
 #endif /* _ASM_X86_CPUFEATURES_H */
