edba96597d6e98304e50382e617adad271d843bb
Andi Kleen <ak@linux.intel.com>
2018-12-12 16:49:56 -0800

    x86/speculation/mds: Schedule cpu clear on context switch
    On context switch we need to schedule a cpu clear on the next
    kernel exit when:
    
    - We're switching between different processes
    - We're switching from a kernel thread that is not idle.
    For idle we assume only interrupts are sensitive, which
    are already handled elsewhere. For kernel threads
    like work queue we assume they might contain
    sensitive (other user's or crypto) data.
    
    The code hooks into the generic context switch, not
    the mm context switch, because the mm context switch
    doesn't handle the idle thread case.
    
    This also transfers the clear cpu bit to the next task.
    
    Tested-by: Neelima Krishnan <neelima.krishnan@intel.com>
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- arch/x86/kernel/process.h
+++ arch/x86/kernel/process.h
@@ -2,6 +2,7 @@
 //
 // Code shared between 32 and 64 bit
 
+#include <linux/clearcpu.h>
 #include <asm/spec-ctrl.h>
 
 void __switch_to_xtra(struct task_struct *prev_p, struct task_struct *next_p);
@@ -28,6 +29,32 @@ static inline void switch_to_extra(struct task_struct *prev,
 			next_tif &= ~_TIF_SPEC_IB;
 		}
 	}
+
+	/*
+	 * When we switch to a different process, or we switch
+	 * from a kernel thread that was not idle, clear the CPU
+	 * buffers on next kernel exit.
+	 *
+	 * We assume that idle does not touch user data, except
+	 * for interrupts, which schedule their own clears as needed.
+	 * But other kernel threads, like work queues, might
+	 * touch user data, so flush in this case.
+	 *
+	 * This has to be here because switch_mm doesn't get
+	 * called in the kernel thread case.
+	 */
+	if (static_cpu_has(X86_BUG_MDS)) {
+		if (prev->pid && (next->mm != prev->mm || prev->mm == NULL))
+			lazy_clear_cpu();
+		/*
+		 * Also transfer the clearcpu flag from the previous task.
+		 * Can be done non atomically because interrupts are off.
+		 */
+		task_thread_info(next)->status |=
+			task_thread_info(prev)->status & _TIF_CLEAR_CPU;
+		task_thread_info(prev)->status &= ~_TIF_CLEAR_CPU;
+	}
+
 
 	/*
 	 * __switch_to_xtra() handles debug registers, i/o bitmaps,
