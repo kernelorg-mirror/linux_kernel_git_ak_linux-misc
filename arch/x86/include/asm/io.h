98f7c0f06b1e36a30b1f45e92890bd1ac84cd4e8
Andi Kleen <ak@linux.intel.com>
2018-12-13 11:28:23 -0800

    mds: Clear cpu for string io/memcpy_*io in interrupts
    Schedule a clear cpu on next kernel exit for string PIO
    or memcpy_from/to_io calls, when they are called in
    interrupts.
    
    The PIO case is likely already handled by old drivers
    not opting in their interrupt handlers to not clear,
    but let's do it just to be sure.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- arch/x86/include/asm/io.h
+++ arch/x86/include/asm/io.h
@@ -40,6 +40,7 @@
 
 #include <linux/string.h>
 #include <linux/compiler.h>
+#include <linux/clearcpu.h>
 #include <asm/page.h>
 #include <asm/early_ioremap.h>
 #include <asm/pgtable_types.h>
@@ -321,6 +322,7 @@ static inline void outs##bwl(int port, const void *addr, unsigned long count) \
 			     : "+S"(addr), "+c"(count)			\
 			     : "d"(port) : "memory");			\
 	}								\
+	lazy_clear_cpu_interrupt();					\
 }									\
 									\
 static inline void ins##bwl(int port, void *addr, unsigned long count)	\
@@ -337,6 +339,7 @@ static inline void ins##bwl(int port, void *addr, unsigned long count)	\
 			     : "+D"(addr), "+c"(count)			\
 			     : "d"(port) : "memory");			\
 	}								\
+	lazy_clear_cpu_interrupt();					\
 }
 
 BUILDIO(b, b, char)
