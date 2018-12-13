5fc3209d2880402d332ec93cf076467b3706a401
Andi Kleen <ak@linux.intel.com>
2018-12-12 16:44:07 -0800

    mds: Instrument skb functions to clear cpu automatically
    Instrument some strategic skbuff functions that either touch
    packet data directly, or are likely followed by a user
    data touch like a memcpy, to schedule a cpu clear on next
    kernel exit. This is only done inside interrupts,
    outside we assume it only touches the current processes' data.
    
    In principle network data should be encrypted anyways,
    but it's better to not leak it.
    
    This provides protection for the network softirq.
    
    Needs more auditing.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- include/linux/skbuff.h
+++ include/linux/skbuff.h
@@ -40,6 +40,7 @@
 #include <linux/in6.h>
 #include <linux/if_packet.h>
 #include <net/flow.h>
+#include <linux/clearcpu.h>
 
 /* The interface for checksum offload between the stack and networking drivers
  * is as follows...
@@ -2093,6 +2094,7 @@ static inline void *__skb_put(struct sk_buff *skb, unsigned int len)
 	SKB_LINEAR_ASSERT(skb);
 	skb->tail += len;
 	skb->len  += len;
+	lazy_clear_cpu_interrupt();
 	return tmp;
 }
 
