697a8918b8203734d1e101b12db7c62f41e7b5de
Andi Kleen <ak@linux.intel.com>
2018-12-12 16:49:56 -0800

    x86/speculation/mds: Schedule cpu clear on context switch
    On context switch we need to schedule a cpu clear on the next
    kernel exit when:
    
    - We're switching between different processes
    - We're switching from a kernel thread
    For kernel threads like work queue assume they might contain
    sensitive (other user's or crypto) data.
    
    The code hooks into the generic context switch, not
    the mm context switch, because the mm context switch
    doesn't handle the kernel thread case.
    
    This also transfers the clear cpu bit to the next task.
    
    Tested-by: Neelima Krishnan <neelima.krishnan@intel.com>
    Signed-off-by: Andi Kleen <ak@linux.intel.com>
    
    ---
    
    v2: Clear on idle too.

--- arch/x86/kernel/process.h
+++ arch/x86/kernel/process.h
@@ -2,6 +2,7 @@
 //
 // Code shared between 32 and 64 bit
 
+#include <linux/clearcpu.h>
 #include <asm/spec-ctrl.h>
 
 void __switch_to_xtra(struct task_struct *prev_p, struct task_struct *next_p);
@@ -28,6 +29,30 @@ static inline void switch_to_extra(struct task_struct *prev,
 			next_tif &= ~_TIF_SPEC_IB;
 		}
 	}
+
+	/*
+	 * When we switch to a different process, or we switch
+	 * from a kernel thread, clear the CPU buffers on next kernel exit.
+	 *
+	 * This has to be here because switch_mm doesn't get
+	 * called in the kernel thread case.
+	 *
+	 * We flush when switching from idle too because idle
+	 * might inherit some leaked data from the SMT sibling.
+	 * This could be optimized for the SMT off case.
+	 */
+	if (static_cpu_has(X86_BUG_MDS)) {
+		if (next->mm != prev->mm || prev->mm == NULL)
+			lazy_clear_cpu();
+		/*
+		 * Also transfer the clearcpu flag from the previous task.
+		 * Can be done non atomically because interrupts are off.
+		 */
+		task_thread_info(next)->status |=
+			task_thread_info(prev)->status & _TIF_CLEAR_CPU;
+		task_thread_info(prev)->status &= ~_TIF_CLEAR_CPU;
+	}
+
 
 	/*
 	 * __switch_to_xtra() handles debug registers, i/o bitmaps,
