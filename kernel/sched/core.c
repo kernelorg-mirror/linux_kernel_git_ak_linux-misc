0baa13e0fdc84704648a70a15408eb543a6b5158
Andi Kleen <ak@linux.intel.com>
2018-12-14 17:26:14 -0800

    mds: Force clear cpu on kernel preemption
    When the kernel is preempted we need to force a cpu clear,
    because the preemption might happen before the code
    has a chance to set TIF_CPU_CLEAR later.
    
    We cannot rely on kernel code setting the flag before
    touching sensitive data: the flag setting could
    be implicit, like in memzero_explicit, which is always
    called later.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -11,6 +11,8 @@
 
 #include <linux/kcov.h>
 
+#include <linux/clearcpu.h>
+
 #include <asm/switch_to.h>
 #include <asm/tlb.h>
 
@@ -3631,6 +3633,13 @@ asmlinkage __visible void __sched notrace preempt_schedule(void)
 	 */
 	if (likely(!preemptible()))
 		return;
+
+	/*
+	 * For kernel preemption we need to force a cpu clear
+	 * because it could happen before the code has a chance
+	 * to set TIF_CLEAR_CPU.
+	 */
+	lazy_clear_cpu();
 
 	preempt_schedule_common();
 }
