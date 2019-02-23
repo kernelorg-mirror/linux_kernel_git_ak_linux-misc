a0e7c4069a092ceb651cae090c5d3cfb6f2af003
Andi Kleen <ak@linux.intel.com>
2019-02-22 17:45:59 -0800

    perf tools report: Implement browsing of individual samples
    Now report can show whole time periods with perf script,
    but the user still has to find individual samples of interest
    manually.
    
    It would be expensive and complicated to search for the
    right samples in the whole perf file. Typically users
    only need to look at a small number of samples for useful
    analysis.
    
    Also the full scripts tend to show samples of all CPUs and all
    threads mixed up, which can be very confusing on larger systems.
    
    Add a new --samples option to save a small random number of samples
    per hist entry
    
    Use a reservoir sample technique to select a representatve
    number of samples.
    
    Then allow browsing the samples using perf script
    as part of the hist entry context menu. This automatically
    adds the right filters, so only the thread or cpu of the sample
    is displayed. Then we use less' search functionality
    to directly jump the to the time stamp of the selected
    sample.
    
    It uses different menus for assembler and source display.
    Assembler needs xed installed and source needs debuginfo.
    
    Currently it only supports as many samples as fit on
    the screen due to some limitations in the slang ui code.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>
    
    ---
    v2:
    Free names on error path
    Pass --inline and --show-*-event to child perf as needed.
    v3:
    Don't use num_res to keep track of total samples
    Don't pass -1 cpus to perf script.
    Don't pass extra event option with tid filtering.
    Fix sampling bug.
    v4:
    Add perfconfig option for context
    Use _PER_NSEC defines
    v5:
    Rebase
    Free res_samples

--- tools/perf/util/hist.c
+++ tools/perf/util/hist.c
@@ -436,6 +436,13 @@ static int hist_entry__init(struct hist_entry *he,
 			goto err_rawdata;
 	}
 
+	if (symbol_conf.res_sample) {
+		he->res_samples = calloc(sizeof(struct res_sample),
+					symbol_conf.res_sample);
+		if (!he->res_samples)
+			return -ENOMEM;
+	}
+
 	INIT_LIST_HEAD(&he->pairs.node);
 	thread__get(he->thread);
 	he->hroot_in  = RB_ROOT_CACHED;
@@ -603,6 +610,32 @@ out:
 	return he;
 }
 
+static unsigned random_max(unsigned high)
+{
+	unsigned thresh = -high % high;
+	for (;;) {
+		unsigned r = random();
+		if (r >= thresh)
+			return r % high;
+	}
+}
+
+static void hists__res_sample(struct hist_entry *he, struct perf_sample *sample)
+{
+	struct res_sample *r;
+	int j;
+
+	if (he->num_res < symbol_conf.res_sample) {
+		j = he->num_res++;
+	} else {
+		j = random_max(symbol_conf.res_sample);
+	}
+	r = &he->res_samples[j];
+	r->time = sample->time;
+	r->cpu = sample->cpu;
+	r->tid = sample->tid;
+}
+
 static struct hist_entry*
 __hists__add_entry(struct hists *hists,
 		   struct addr_location *al,
@@ -650,6 +683,8 @@ __hists__add_entry(struct hists *hists,
 
 	if (!hists->has_callchains && he && he->callchain_size != 0)
 		hists->has_callchains = true;
+	if (he && symbol_conf.res_sample)
+		hists__res_sample(he, sample);
 	return he;
 }
 
@@ -1173,6 +1208,7 @@ void hist_entry__delete(struct hist_entry *he)
 		mem_info__zput(he->mem_info);
 	}
 
+	zfree(&he->res_samples);
 	zfree(&he->stat_acc);
 	free_srcline(he->srcline);
 	if (he->srcfile && he->srcfile[0])
