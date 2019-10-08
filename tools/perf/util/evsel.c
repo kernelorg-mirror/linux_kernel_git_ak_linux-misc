10be4df5e90a1c3d555fd5effa9cf9b04c68b0ef
Andi Kleen <ak@linux.intel.com>
2019-10-08 12:18:02 -0700

    perf evsel: Avoid close(-1)
    In some weak fallback cases close can be called a lot with -1. Check
    for this case and avoid calling close then.
    
    This is mainly to shut up valgrind which complains about this case.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- tools/perf/util/evsel.c
+++ tools/perf/util/evsel.c
@@ -1981,7 +1981,8 @@ out_close:
 	old_errno = errno;
 	do {
 		while (--thread >= 0) {
-			close(FD(evsel, cpu, thread));
+			if (FD(evsel, cpu, thread) >= 0)
+				close(FD(evsel, cpu, thread));
 			FD(evsel, cpu, thread) = -1;
 		}
 		thread = nthreads;
