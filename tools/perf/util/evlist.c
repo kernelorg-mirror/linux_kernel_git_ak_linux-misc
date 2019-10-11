94ddb68505cd6008964f300ce36bf97754ba904b
Andi Kleen <ak@linux.intel.com>
2019-10-11 13:12:21 -0700

    perf stat: Use affinity for closing file descriptors
    Closing a perf fd can also trigger an IPI to the target CPU.
    Use the same affinity technique as we use for reading/enabling events
    to closing to optimize the CPU transitions.
    
    Before on a large test case with 94 CPUs:
    
    % time     seconds  usecs/call     calls    errors syscall
    ------ ----------- ----------- --------- --------- ----------------
     32.56    3.085463          50     61483           close
    
    After:
    
     10.54    0.735704          11     61485           close
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>
    
    ---
    
    v2: Use new iterator macros
    v3: Use new iterator macros
    Add missing affinity__cleanup

--- tools/perf/util/evlist.c
+++ tools/perf/util/evlist.c
@@ -18,6 +18,7 @@
 #include "debug.h"
 #include "units.h"
 #include <internal/lib.h> // page_size
+#include "affinity.h"
 #include "../perf.h"
 #include "asm/bug.h"
 #include "bpf-event.h"
@@ -1165,9 +1166,33 @@ void perf_evlist__set_selected(struct evlist *evlist,
 void evlist__close(struct evlist *evlist)
 {
 	struct evsel *evsel;
+	struct affinity affinity;
+	int cpu, i;
 
-	evlist__for_each_entry_reverse(evlist, evsel)
-		evsel__close(evsel);
+	if (!evlist->core.cpus) {
+		evlist__for_each_entry_reverse(evlist, evsel)
+			evsel__close(evsel);
+		return;
+	}
+
+	if (affinity__setup(&affinity) < 0)
+		return;
+	evlist__cpu_iter_start(evlist);
+	evlist__for_each_cpu (evlist, i, cpu) {
+		affinity__set(&affinity, cpu);
+
+		evlist__for_each_entry_reverse(evlist, evsel) {
+			if (evlist__cpu_iter_skip(evsel, cpu))
+			    continue;
+			perf_evsel__close_cpu(&evsel->core, evsel->cpu_index);
+			evlist__cpu_iter_next(evsel);
+		}
+	}
+	affinity__cleanup(&affinity);
+	evlist__for_each_entry_reverse(evlist, evsel) {
+		perf_evsel__free_fd(&evsel->core);
+		perf_evsel__free_id(&evsel->core);
+	}
 }
 
 static int perf_evlist__create_syswide_maps(struct evlist *evlist)
