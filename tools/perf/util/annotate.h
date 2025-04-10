6786e4152160de154c12283df2f76959d38734a6
Andi Kleen <ak@linux.intel.com>
2025-04-10 15:18:12 -0700

    fixup! perf annotate-data: Refactor high level type functions to be more independent of annotate

--- tools/perf/util/annotate.h
+++ tools/perf/util/annotate.h
@@ -594,8 +594,6 @@ int annotation_br_cntr_entry(char **str, int br_cntr_nr, u64 *br_cntr,
 			     int num_aggr, struct evsel *evsel);
 int annotation_br_cntr_abbr_list(char **str, struct evsel *evsel, bool header);
 
-struct thread;
-struct debuginfo;
 
 #define NO_TYPE ((struct annotated_data_type *)-1UL)
 
