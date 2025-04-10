62223169cd651b9d082a328547bfd4f1a64e91db
Andi Kleen <ak@linux.intel.com>
2025-04-10 11:09:25 -0700

    fixup! perf test: Add simple test for data-type perf script

--- tools/perf/tests/shell/script-type.sh
+++ tools/perf/tests/shell/script-type.sh
@@ -1,35 +1,75 @@
-#!/bin/sh
+#!/bin/bash
+# some tests for type resolution
 set -e
 PERF=${PERF:-perf}
 CC=${CC:-gcc}
 
-if ! $PERF check feature -q libcapstone ; then exit 0 ; fi
-if ! $PERF check feature -q dwarf ; then exit 0 ; fi
+if ! $PERF check feature -q libcapstone ; then
+	echo "SKIP: libcapstone is not supported"
+	exit 0
+fi
+if ! $PERF check feature -q dwarf ; then
+	echo "SKIP: dwarf is not supported"
+	exit 0
+fi
 
 cleanup() {
-	rm -f ttype$$.c ttype$$ type$$.data type$$.script type$$b.script type$$b.data type$$.report
+	if [ -z "$TYPE_KEEP_OUTPUT" ] ; then
+		rm -f type$$ type$$.{c,data,report,annotate,script} \
+		       	type$$b.{data,report,annotate,script}
+	fi
 }
 
-cat >ttype$$.c <<EOL
-volatile int v;
+trap_cleanup() {
+	echo FAILED
+	cleanup
+	exit 1
+}
+
+trap trap_cleanup EXIT TERM INT
+
+cat >type$$.c <<EOL
+int v;
 int main()
 {
 	int i;
-	for (i = 0; i < 100000; i++)
+	for (i = 0; i < 4000000; i++) {
 		v++;
+		asm("" :: "r" (v) : "memory");
+	}
 }
 EOL
 
-$CC ttype$$.c -O2 -g -o ttype$$
-$PERF record -q -o type$$.data -e cycles:u ./ttype$$
-$PERF annotate --code-with-type -i type$$.data --stdio > type$$.report
+$CC type$$.c -O2 -g -o type$$
+$PERF record -c 10000 -q -o type$$.data -e cycles:u ./type$$
+$PERF annotate --code-with-type -i type$$.data --stdio main > type$$.annotate
+$PERF report --sort type,typeoff,typecln -i type$$.data > type$$.report
 $PERF script -i type$$.data -F +disasm,+data_type > type$$.script
 
-# XXX some output checking
+grep -q int type$$.report
+grep -q "data-type: int" type$$.annotate
 
-if $PERF record -q -o /dev/null -b true ; then
-	$PERF record -q -o type$$b.data -e cycles:u -b ./ttype$$
+# XXX int for script
+
+# needs more samples:
+#grep -q "stack operation" type$$.script
+
+echo "script disasm with data_type did not crash [Success]"
+
+if $PERF record -q -c 10000 -o /dev/null -b true ; then
+	$PERF record -q -o type$$b.data -e cycles:u -b ./type$$
+	$PERF report --sort type,typeoff,typecln -i type$$b.data > type$$b.report
+	$PERF annotate --code-with-type -i type$$b.data --stdio main > type$$b.annotate
 	$PERF script -i type$$b.data -F +brstackdisasm,+data_type > type$$b.script
+
+	# does not report anything?
+	#grep -q int type$$b.report
+	grep -q "data-type: int" type$$b.annotate
+
+	echo "script brstackdisasm with data-type did not crash [Success]"
+else
+	echo "SKIP: branch recording is not supported"
 fi
 
 cleanup
+trap - EXIT TERM INT
